6016ae8310d42690cb0bcba3031a098f33d305f5
lut777 <tony.leu777@gmail.com>
2020-11-10 14:17:22 +0800

    add cr spec

--- api/v1/appoperator_types.go
+++ api/v1/appoperator_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,14 +29,28 @@ type AppOperatorSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of AppOperator. Edit AppOperator_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// corev1 "k8s.io/api/core/v1"
+	// Check that resources.requests.storage must be configured
+	corev1.ResourceRequirements `json:"resources"`
+	// Address must be a valid URL
+	Address string `json:"address"`
+	// User must be with format of `a_b` (alphanumeric characters, separated by only one `_`)
+	User string `json:"user,omitempty"`
+	// Type can only be `full` or `partial`.
+	Type string `json:"type,omitempty"`
+	// StorageSize must be a valid Quantity (https://godoc.org/k8s.io/apimachinery/pkg/api/resource#Quantity)
+	// and must be greater than resources.requests.storage
+	StorageSize string `json:"storageSize,omitempty"`
+	// MaxBackups must be >=1 && <= 10.
+	MaxBackups int32 `json:"maxBackups"`
+
 }
 
 // AppOperatorStatus defines the observed state of AppOperator
 type AppOperatorStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Path string `json:"path"`
 }
 
 // +kubebuilder:object:root=true
@@ -43,10 +58,10 @@ type AppOperatorStatus struct {
 // AppOperator is the Schema for the appoperators API
 type AppOperator struct {
 	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.ObjectMeta `json:"metadata"`
 
-	Spec   AppOperatorSpec   `json:"spec,omitempty"`
-	Status AppOperatorStatus `json:"status,omitempty"`
+	Spec   AppOperatorSpec   `json:"spec"`
+	Status AppOperatorStatus `json:"status"`
 }
 
 // +kubebuilder:object:root=true
